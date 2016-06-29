067238c6c5ea47a5f9fd1c6a7d126b7a83e96d66
Sanctuar <it.security@list.ru>
2016-06-29 23:02:59 +0300

    Changes

--- massive_code/massive_code/Site1.Master.cs
+++ massive_code/massive_code/Site1.Master.cs
@@ -11,7 +11,14 @@ namespace massive_code
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Button_Download_UnionServer.Attributes.Add("onmouseover", "this.className='button_mouse_over1'");
+            Button_Download_UnionServer.Attributes.Add("onmouseout", "this.className='button_mouse_out1'");
+            Button_Download_UnionClient.Attributes.Add("onmouseover", "this.className='button_mouse_over1'");
+            Button_Download_UnionClient.Attributes.Add("onmouseout", "this.className='button_mouse_out1'");
+        }
+        public int pi_ClientScreenWidth()
+        {
+            return Request.Browser.ScreenPixelsWidth;
         }
     }
 }
